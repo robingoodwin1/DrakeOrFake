242654b650945d011848aaab355e73d4da596d7b
robingoodwin1 <18goodro@wcsu.net>
2017-06-08 08:24:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,106 @@
+var gameMode = 0;  // 0 = welcome screen, 1 = game screen; 2 = score screen; 3 = timeout screen; 4 = youlose
+
+var DrakeNumber;
+var score = 0;
+var t;
+var Drake;
+
+function setup() {
+  createCanvas(800, 600);
+  noLoop();
+}
+
+function draw() {
+  if (gameMode == 0) {
+    // display welcome screen
+    background(225, 0, 0);
+    textFont("Georgia")
+    textSize(50);
+    fill(0, 0, 0);
+    text("Welcome to Drake or Fake!", 90, 280);
+    textSize(30);
+    text("Press any key to begin.", 240, 320);
+
+  } else if (gameMode == 1) {
+    // display game screen
+    background(225, 0, 0);
+    text("score = " + score, 340, 575);
+     setTimeout(function() {
+       gameMode = 2;
+     }, 60000);
+     DrakeNumber = parseInt(random(1, 21));
+     var img = loadImage(DrakeNumber + ".jpg");
+     image(img, 40, 40);
+     text(DrakeNumber, 200, 200);
+
+   } else if (gameMode == 2) {
+     // display score screen
+     background(255);
+     text("what is this screen?", 10, 100);
+   } else if (gameMode == 3) {
+    // display timeout screen
+    background(255);
+    text("T to the I to the M to the E", 10, 100);
+  } else if (gameMode == 4) {
+    // display youlose screen
+    background(200, 50, 50);
+    textSize(50);
+    text("Wrong choice! You lose.", 120, 280);
+    textSize(30);
+    text("Press the 'a' key to play again.", 200, 315);
+      }
+    }
+
+function keyTyped() {
+  if (gameMode == 0) {
+    //keys to listen for in welcome screen
+    gameMode = 1;
+    redraw();
+  } else if (gameMode == 1) {
+    //keys to listen for in game screen
+    if (key == 'd') {
+      console.log(DrakeNumber);
+      var Drake = "DRAKE!"
+      console.log(Drake)
+       if (DrakeNumber >= 12) {
+         score = score + 1;
+         console.log("CORRECT SELECTION!");
+         gameMode = 1;
+         redraw();
+       } else {
+         score = 0;
+         console.log("INCORRECT SELECTION!");
+         gameMode = 4;
+         redraw();
+       }
+    } else if (key == 'f') {
+        console.log(DrakeNumber)
+        var Drake = "FAKE!"
+        console.log(Drake)
+      if (DrakeNumber < 12) {
+         score = score + 1;
+         console.log("CORRECT SELECTION!");
+         gameMode = 1;
+         redraw();
+       } else {
+         score = 0;
+         console.log("INCORRECT SELECTION!");
+         gameMode = 4;
+         redraw();
+       }
+    } else {
+      console.log("invalid key");
+    }
+  } else if (gameMode == 4) {
+    //keys to listen for in youlose screen
+    if (key == 'a') {
+      console.log("restarting");
+      gameMode = 0;
+      redraw();
+    } else if (key == 'b') {
+      console.log("bee movie");
+    } else {
+      console.log(key + " is an invalid key");
+    }
+  }
+}
